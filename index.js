6cae9dbabdeca1454541d4c43f2832e6a8ae3e7d
Princewill <princesolo919@gmail.com>
2022-04-07 12:45:25 +0100

    added mongoose schemas and routes

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const userRoute = require('./routes/user')
+dotenv.config()
+
+const app = express()
+
+mongoose.connect(
+    process.env.Mongokey
+    ).then(()=> console.log('successful'))
+    .catch((err) => {
+        console.log(err)
+    })
+
+
+
+app.use(express.json())
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+
+app.use('/api/users', userRoute)
+
+app.listen(2000, function () {
+    console.log('started')
+})
---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get('/usertest', (req,res) => {
+    res.send('success')
+})
+
+router.post('/usertest', (req,res) => {
+    const username = req.body.username
+    console.log(username)
+})
+
+module.exports = router
